493e0752ca42b0fd6de16bee701d802e950e0c0d
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-09-11 21:46:36 -0300

    Correção UserController

--- src/interfaces/controllers/userController.js
+++ src/interfaces/controllers/userController.js
@@ -15,11 +15,7 @@ const userController = {
     try {
       const CreatedUser = await CreateUser(req.body, userRepository);
 
-      await ConfirmationEmail(
-        CreatedUser._id,
-        CreatedUser.email,
-        userRepository
-      );
+      await ConfirmationEmail(CreatedUser._id, userRepository);
 
       return res.status(201).json(CreatedUser);
     } catch (error) {
