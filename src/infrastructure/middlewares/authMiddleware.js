89bc90839798e715cddb2555c3fcda13e5a7a671
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-12-14 19:50:31 -0300

    Erro 450

--- src/infrastructure/middlewares/authMiddleware.js
+++ src/infrastructure/middlewares/authMiddleware.js
@@ -4,19 +4,19 @@ function authenticationMiddleware(req, res, next) {
   const token = req.header("Authorization");
 
   if (!token) {
-    return res.status(401).json({ error: "Access denied. Token missing." });
+    return res.status(450).json({ error: "Access denied. Token missing." });
   }
 
   try {
     const decoded = jwtService.verifyToken(token);
     if (!decoded) {
-      return res.status(401).json({ error: "Access denied. Invalid token." });
+      return res.status(450).json({ error: "Access denied. Invalid token." });
     }
 
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({ error: "Access denied. Invalid token." });
+    return res.status(450).json({ error: "Access denied. Invalid token." });
   }
 }
 
