d3188e0ecd1a6439da73f65fb7295d4368489a41
Gabriel Botelho <gabrielbbotelho05@gmail.com>
2023-12-17 11:50:31 -0300

    Usuarios - mudar senha #2

--- src/application/useCases/user/UpdatePassword.js
+++ src/application/useCases/user/UpdatePassword.js
@@ -1,7 +1,6 @@
 const bcrypt = require("bcrypt");
 
 module.exports = async (userId, passwordData, userRepository) => {
-  console.log("Passou");
   try {
     // Verifica se o usuário existe
     const existingUser = await userRepository.getById(userId);
@@ -9,27 +8,30 @@ module.exports = async (userId, passwordData, userRepository) => {
       throw { message: "User not found" };
     }
 
-    // Verifica se a senha atual fornecida corresponde à senha armazenada
-    const passwordMatch = await bcrypt.compare(
-      passwordData.currentPassword,
-      existingUser.password
-    );
-    if (!passwordMatch) {
-      throw { message: "Senha atual incorreta!" };
+    // Verifica se a senha atual fornecida corresponde à senha armazenada,
+    // a menos que o usuário seja um admin (admin === 1)
+    if (passwordData.admin === 0) {
+      const passwordMatch = await bcrypt.compare(
+        passwordData.currentPassword,
+        existingUser.password
+      );
+      if (!passwordMatch) {
+        throw { message: "Senha atual incorreta!" };
+      }
+    } else {
+      // Criptografa a nova senha
+      const hashedNewPassword = await bcrypt.hash(passwordData.newPassword, 10);
+
+      // Atualiza a senha do usuário
+      existingUser.password = hashedNewPassword;
+
+      // Executa a atualização no repositório
+      const updatedUser = await userRepository.update(userId, {
+        password: hashedNewPassword,
+      });
+
+      return updatedUser;
     }
-
-    // Criptografa a nova senha
-    const hashedNewPassword = await bcrypt.hash(passwordData.newPassword, 10);
-
-    // Atualiza a senha do usuário
-    existingUser.password = hashedNewPassword;
-
-    // Executa a atualização no repositório
-    const updatedUser = await userRepository.update(userId, {
-      password: hashedNewPassword,
-    });
-
-    return updatedUser;
   } catch (error) {
     throw { error: error.message || "Failed password update" };
   }
